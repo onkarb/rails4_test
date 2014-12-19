0771ad230508b1a144cf904646477a11a59eb4ec
onkarbellamkonda <onkarb99@gmail.com>
2014-12-19 19:15:07 +0530

    added schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923072542) do
+ActiveRecord::Schema.define(version: 20141219124136) do
+
+  create_table "books", force: true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.date     "released_on"
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
